9bc22b95895537b94f8e57a5ea5cfc78d05a8797
LorenzoBuongiorno <lorenzo.buongiorno00@gmail.com>
2022-02-17 16:29:50 +0100

    created albums in database

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include "database/albums.php";
+ var_dump($albums);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-ajax-dischi</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
